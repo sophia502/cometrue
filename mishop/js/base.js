f72673b7340f2b0a33ce18c6d7eca6bcf03954b5
sophia502 <1848518984@qq.com>
2018-03-28 18:52:10 +0800

    mishop 仿写小米商城

--- mishop/js/base.js
+++ mishop/js/base.js
@@ -1,4 +1,10 @@
+var oChart = document.getElementById('chart');
+var aImg = oChart.getElementsByTagName('img');
+var aLi = oChart.getElementsByTagName('li');
+var oPreBtn = document.getElementById('ib1');
+var oNextBtn = document.getElementById('ib2');
 var oBtn = document.getElementById('go-top');
+var nowIndex = 0;
 oBtn.onclick = function(){
     var iScrollTop = document.documentElement.scrollTop ||document.body.scrollTop;
     var timer = setInterval(function () {
@@ -8,3 +14,45 @@ oBtn.onclick = function(){
         }
     },100);
 };
+for (var i=0;i<aLi.length;i++){
+    aLi[i].index = i;
+    aLi[i].onmouseover = function () {
+        nowIndex = this.index;
+        changeImg();
+    }
+};
+oNextBtn.onclick = oPreBtn.onclick = function () {
+    if(this==oPreBtn){
+        nowIndex--;
+        if(nowIndex == -1){
+            nowIndex = aLi.length-1;
+        }
+    }else{
+        nowIndex++;
+        if(nowIndex == aLi.length){
+            nowIndex = 0;
+        }
+    }
+    changeImg();
+};
+var timer = setInterval(function () {
+    oNextBtn.onclick();
+},1000);
+console.log(oChart);
+oChart.onmouseover = function () {
+    clearInterval(timer);
+};
+oChart.onmouseout = function () {
+    timer =setInterval(function () {
+        oNextBtn.onclick();
+    },1000);
+};
+function changeImg() {
+    for (var j=0;j<aLi.length;j++){
+        aLi[j].className = '';
+        aImg[j].className = '';
+    }
+    aLi[nowIndex].className = "selected";
+    aImg[nowIndex].className = 'selected';
+}
+
